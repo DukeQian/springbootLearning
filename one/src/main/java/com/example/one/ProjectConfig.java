a5bef5de2c6aaf907f093888080ad839d818e899
yunlong.qian <yunlong.qian>
2017-09-06 16:03:42 +0800

    1 关于项目配置

---
+++ one/src/main/java/com/example/one/ProjectConfig.java
@@ -0,0 +1,29 @@
+package com.example.one;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "projectConfig")
+public class ProjectConfig {
+
+    private String one;
+
+    private String two;
+
+    public String getOne() {
+        return one;
+    }
+
+    public void setOne(String one) {
+        this.one = one;
+    }
+
+    public String getTwo() {
+        return two;
+    }
+
+    public void setTwo(String two) {
+        this.two = two;
+    }
+}
